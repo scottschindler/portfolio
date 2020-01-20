576072b3b24889dc73d5e44db877e84a6d219ecc
Scott Schindler <scottschindler29@gmail.com>
2020-01-20 13:00:59 -0500

    scott making changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,5 +24,8 @@ export default () => (
     <p> Chaning more things. Changing more things. </p>
     <p> Chaning more things. Changing more things. </p>
     <p> Chaning more things. Changing more things. </p>
+    <p> Chaning more things. Changing more things. </p>
+    <p> Chaning more things. Changing more things. </p>
+    <p> Chaning more things. Changing more things. </p>
   </div>
 )
